7d352873162fe4209f4a25ace2bc07999b8e395c
Josh Dorn <jr_dorn@yahoo.com>
2021-12-09 21:55:43 -0600

    Test timer animation

--- bin/quickbite.mjs
+++ bin/quickbite.mjs
@@ -38,5 +38,15 @@ console.log(
 \n`)
 );
 
+//sample animation
+var twirlTimer = (function () {
+  var P = ["\\", "|", "/", "-"];
+  var x = 0;
+  return setInterval(function () {
+    process.stdout.write("\r" + P[x++]);
+    x &= 3;
+  }, 250);
+})();
+
 // cli(chalk.yellow(process.argv[2]));
 if (options.peppers) console.log("peppers");
