7731c384155f89c3c5854b5f6d224b6ef8659562
Josh Dorn <jr_dorn@yahoo.com>
2022-02-09 19:52:53 -0600

    Test directory

---
+++ test/sum.test.js
@@ -0,0 +1,50 @@
+const sum = require("./sum");
+
+//test with exact equality
+test("adds 1+2=3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+/**
+ 
+expect() expectation object
+toBe() matcher
+
+toEqual to check value of an object
+
+ */
+
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+//test for opposite of a matcher
+test("adding positive nums is not zero", () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
+});
+
+//truthiness
+test("null", () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+test("zero", () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy;
+});
+
+//Numbers
