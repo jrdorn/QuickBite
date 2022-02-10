2ca0bcaa691c6e9a5c371ed1bd4044c3982be065
Josh Dorn <jr_dorn@yahoo.com>
2022-02-10 13:21:11 -0600

    Temp dummy test

--- test/testAPI.test.js
+++ test/testAPI.test.js
@@ -1,6 +1,22 @@
 const testAPI = require("./testAPI");
 
-//
-test("init Jest API call", () => {
-  expect(testAPI()).toBe({ one: "1", two: "2" });
+//async test
+// test("init Jest API call", () => {
+//   return testAPI().then((result) => {
+//     expect(result).toEqual({ one: "1", two: "2" });
+//   });
+// });
+
+//dummy test
+//Numbers
+test("two plus two", () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+
+  // toBe and toEqual are the same for numbers
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
 });
